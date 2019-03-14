9d04936190af954a6d2acc928c0dcd9ef60efcb7
mattew <mattew@mattews-MacBook-Air.local>
2019-03-15 01:03:04 +0330

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.PYPI.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -9,12 +9,12 @@ setup(
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	packages=['ezcolor'],
-	version='0.3',
+	version='0.2',
 	description='A lightweight lib for adding extra attributes to text',
 	author='Fardin Allahverdinazhand',
 	author_email='0x0ptim0us@gmail.com',
 	url='https://github.com/0x0ptim0us/ezcolor',
-	download_url='https://github.com/0x0ptim0us/ezcolor/archive/0.3.tar.gz',
+	download_url='https://github.com/0x0ptim0us/ezcolor/archive/0.2.tar.gz',
 	keywords=['python3', 'colorize', 'ezcolor', 'output attribute', 'output colorize', 'text color', 'color'],
 	classifiers=[],
 )
