69cc7d764989150bfa251daada9dd473eff18d30
mattew <mattew@mattews-MacBook-Air.local>
2019-03-15 01:24:21 +0330

    fix setup

--- pypi-readme.md
+++
@@ -1,6 +0,0 @@
-#### A lightweight lib for adding extra attributes to text
-##### Installation :
-pip3 install ezcolor
-
-##### Github page:
-https://github.com/0x0ptim0us/ezcolor
--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from distutils.core import setup
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'pypi-readme.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
 	name='ezcolor',
-	long_description=long_description,
-	long_description_content_type='text/markdown',
 	packages=['ezcolor'],
-	version='0.4',
+	version='0.5',
 	description='A lightweight lib for adding extra attributes to text',
 	author='Fardin Allahverdinazhand',
 	author_email='0x0ptim0us@gmail.com',
 	url='https://github.com/0x0ptim0us/ezcolor',
-	download_url='https://github.com/0x0ptim0us/ezcolor/archive/0.4.tar.gz',
+	download_url='https://github.com/0x0ptim0us/ezcolor/archive/0.5.tar.gz',
 	keywords=['python3', 'colorize', 'ezcolor', 'output attribute', 'output colorize', 'text color', 'color'],
 	classifiers=[],
 )
