c4515f09a9712bf5c7b4f231c3c637ed19cd123c
mattew <mattew@mattews-MacBook-Air.local>
2019-03-14 19:58:08 +0330

    Fix PEP8

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+
 setup(
-  name = 'ezcolor',
-  packages = ['ezcolor'], 
-  version = '0.2',
-  description = 'A lightweight lib for adding extra attributes to text',
-  author = 'Fardin Allahverdinazhand',
-  author_email = '0x0ptim0us@gmail.com',
-  url = 'https://github.com/0x0ptim0us/ezcolor', 
-  download_url = 'https://github.com/0x0ptim0us/ezcolor/archive/0.2.tar.gz',
-  keywords = ['python3', 'colorize', 'ezcolor', 'output attribute', 'output colorize', 'text color', 'color'],
-  classifiers = [],
+	name='ezcolor',
+	packages=['ezcolor'],
+	version='0.2',
+	description='A lightweight lib for adding extra attributes to text',
+	author='Fardin Allahverdinazhand',
+	author_email='0x0ptim0us@gmail.com',
+	url='https://github.com/0x0ptim0us/ezcolor',
+	download_url='https://github.com/0x0ptim0us/ezcolor/archive/0.2.tar.gz',
+	keywords=['python3', 'colorize', 'ezcolor', 'output attribute', 'output colorize', 'text color', 'color'],
+	classifiers=[],
 )
